4647a5190d222bfff35826f3de65999eb8d41d41
Ha Tuyen <hatuyen18@gmail.com>
2022-12-24 21:38:34 +1100

    Update new challenge

---
+++ Lecture03/FindMinMaxScore.java
@@ -0,0 +1,9 @@
+package Lecture03;
+
+import java.util.Scanner;
+
+public class FindMinMaxScore {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
