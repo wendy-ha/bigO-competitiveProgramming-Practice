6f4a30a408ded7643fdab392d0502de6b7a258c9
Ha Tuyen <hatuyen18@gmail.com>
2022-12-25 18:31:31 +1100

    Update soure code for FindMinMaxScore

--- Lecture03/FindMinMaxScore.java
+++ Lecture03/FindMinMaxScore.java
@@ -5,5 +5,27 @@ import java.util.Scanner;
 public class FindMinMaxScore {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int input;
+        int min = 11;
+        int max = -1;
+
+        while (true) {
+            input = scanner.nextInt();
+            if (input >= 0 && input <=10) {
+                if ( input > max) {
+                    max = input;
+                }
+                if (input <= min) {
+                    min = input;
+                }
+                if(input ==-1){
+                    break;
+                }
+            }
+            else break;
+
+        }
+        System.out.printf("%d %d", max, min);
+
     }
 }
