09106c83a768a15a8f992be05edc609d10ff9c38
Ha Tuyen <hatuyen18@gmail.com>
2022-12-30 23:55:38 +1100

    update code new

--- Lecture04/test3.java
+++ Lecture04/test3.java
@@ -2,6 +2,6 @@ package Lecture04;
 
 public class test3 {
     public static void main(String args[]) {
-        System.out.print("Hello world");
+        System.out.print("Hello world!");
     }
 }
