fc19a5d0f31584970b7b19d18c6d608654674d6b
Ha Tuyen <hatuyen18@gmail.com>
2023-01-07 00:59:00 +1100

    update code

--- Lecture10/FindFurthestPoint.java
+++ Lecture10/FindFurthestPoint.java
@@ -3,4 +3,10 @@ package Lecture10;
 public class FindFurthestPoint {
     int num;
     int denom;
+    public Fraction(int num, int denom) {
+        this.num = num;
+        this.denom = denom;
+    }
+    
+
 }
